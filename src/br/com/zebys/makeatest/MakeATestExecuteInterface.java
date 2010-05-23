0baebbb0bd83e11609012e8ee646e025a7d32035
Marcus Floriano <marcus.floriano@gmail.com>
2010-05-23 09:57:26 -0300

    refactory the classes

--- src/br/com/zebys/makeatest/MakeATestExecuteInterface.java
+++
@@ -1,7 +0,0 @@
-package br.com.zebys.makeatest;
-
-import java.lang.annotation.Annotation;
-
-public interface MakeATestExecuteInterface {
-	public void execute(Annotation annotation) throws Exception;
-}
