2ddae038712facd57b134dcd645e935f08e19187
Marcus Floriano <marcus.floriano@gmail.com>
2010-05-23 07:34:59 -0300

    alter class for annotations types, organizing with methods and add try for newInstance the class executor for verify if extends from MakeATestExecute.class

--- src/br/com/zebys/makeatest/proxy/MakeATestProxy.java
+++ src/br/com/zebys/makeatest/proxy/MakeATestProxy.java
@@ -1,13 +1,16 @@
 package br.com.zebys.makeatest.proxy;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
+import br.com.zebys.makeatest.MakeATestExecuteInterface;
 import br.com.zebys.makeatest.annotations.MakeATestConfig;
+import br.com.zebys.makeatest.exception.MakeATestException;
 
 public class MakeATestProxy implements MethodInterceptor {
 
@@ -19,22 +22,53 @@ public class MakeATestProxy implements MethodInterceptor {
 	
 	@Override
 	public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+		this.methodAnnotations(obj, method, args, proxy);
+		this.fieldAnnotation(obj, method, args, proxy);
+		return method.invoke(this.object, args);
+	}
+	
+	private void fieldAnnotation(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+		Class<?> klass = this.object.getClass();
+		Field [] fields = klass.getDeclaredFields();
+		
+		for (Field field : fields) {
+			Annotation[] annotations = field.getDeclaredAnnotations();
+			for (Annotation annotation : annotations) {
+				if(annotation.annotationType().isAnnotationPresent(MakeATestConfig.class)) {
+					this.invoke(annotation);
+				}
+			}
+		}
+	}
+	
+	private void methodAnnotations(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
 		Annotation[] annotations = method.getDeclaredAnnotations();
 		
 		for (Annotation annotation : annotations) {
 			if(annotation.annotationType().isAnnotationPresent(MakeATestConfig.class)) {
-				MakeATestConfig makeATestConfig = annotation.annotationType().getAnnotation(MakeATestConfig.class);
-            	Class<?> executorClasse = (Class<?>) makeATestConfig.klass();
-                Object executor = executorClasse.newInstance();
-                Method execute = executorClasse.getMethod("execute", Annotation.class);
-                try {
-                	execute.invoke(executor, annotation);
-                } catch (InvocationTargetException e) {
-    	            throw e.getTargetException();
-    	        }
+				this.invoke(annotation);
 			}			
+		}	
+	}
+	
+	private void invoke(Annotation annotation) throws Throwable {
+		MakeATestConfig makeATestConfig = annotation.annotationType().getAnnotation(MakeATestConfig.class);
+    	Class<?> executorClasse = (Class<?>) makeATestConfig.klass();
+    	
+    	try {
+    		executorClasse.asSubclass(MakeATestExecuteInterface.class);
+    	} catch (Exception e) {
+			throw new MakeATestException("The class executing not implements the " + MakeATestExecuteInterface.class.getCanonicalName());
 		}
-		return method.invoke(this.object, args);
+    	
+    	Object executor = executorClasse.newInstance();
+        Method execute = executorClasse.getMethod("execute", Annotation.class);
+        
+    	try {
+        	execute.invoke(executor, annotation);
+        } catch (InvocationTargetException e) {
+            throw e.getTargetException();
+        } 
 	}
 	
 	@SuppressWarnings("unchecked")
