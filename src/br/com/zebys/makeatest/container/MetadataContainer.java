63b0e61efe1d56842662d4d2a7bdda74cfdd571c
Marcus Floriano <marcus.floriano@gmail.com>
2011-01-03 13:03:41 -0200

    documentações

--- src/br/com/zebys/makeatest/container/MetadataContainer.java
+++ src/br/com/zebys/makeatest/container/MetadataContainer.java
@@ -7,17 +7,27 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Container
+ * Responsável por armazenar as informações dos metadados das classes.
  * @author deborachama
+ *
+ * @see PropertyDescriptor
  *
  */
 public class MetadataContainer {
 	private Map<Method, List<PropertyDescriptor>> properties;
 
+	/**
+	 * Construturo para iniciar o {@link PropertyDescriptor}, sendo o mesmo uma uníca instancia.
+	 */
 	public MetadataContainer() {
 		this.properties = new HashMap<Method, List<PropertyDescriptor>>();
 	}
 
+	/**
+	 * Adiciona no container o método como chave e o {@link PropertyDescriptor} nesta chave com as informações das anotações. 
+	 * @param method
+	 * @param propertyDescriptor
+	 */
 	public void put(Method method, PropertyDescriptor propertyDescriptor) {
 		if (!this.properties.containsKey(method)) {
 			List<PropertyDescriptor> props = new ArrayList<PropertyDescriptor>();
@@ -28,10 +38,19 @@ public class MetadataContainer {
 		}
 	}
 
+	/**
+	 * Recupera o MAP com os {@link PropertyDescriptor}
+	 * @return Mapa com as chaves métodos e cada chave representa um {@link PropertyDescriptor}
+	 */
 	public Map<Method, List<PropertyDescriptor>> getProperties() {
 		return this.properties;
 	}
 	
+	/**
+	 * Retorna uma lista de {@link PropertyDescriptor} a patir do do método
+	 * @param method Método que representa a anotação
+	 * @return {@link PropertyDescriptor}
+	 */
 	public List<PropertyDescriptor> getProperties(Method method) {
 		return this.properties.get(method);
 	}
