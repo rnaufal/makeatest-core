1dc3b75fa7f256264d9f55e0528d986ff84a65a2
Debora Chama <dchama@sp.r7.com>
2011-01-18 12:20:27 -0200

    Adicionado metadata processor

---
+++ src/com/yediat/makeatest/metadataprocessor/DefaultMetadataProcessor.java
@@ -0,0 +1,11 @@
+package com.yediat.makeatest.metadataprocessor;
+
+public class DefaultMetadataProcessor implements MetadataProcessor {
+
+	@Override
+	public void process() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/yediat/makeatest/metadataprocessor/MetadataProcessor.java
@@ -0,0 +1,5 @@
+package com.yediat.makeatest.metadataprocessor;
+
+public interface MetadataProcessor {
+	public void process() throws Exception;
+}
