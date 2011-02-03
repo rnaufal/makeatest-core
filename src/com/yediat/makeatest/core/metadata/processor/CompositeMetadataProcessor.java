ba5f4f2f840999564f7937ef30201042032d61db
Marcus Vinicius Costa Floriano <marcusfloriano@Marcus-Floriano.local>
2011-02-03 21:18:08 -0200

    inclusão de componente no eclipse para metricas de código, e verificação do compositemetadaProcessor

---
+++ src/com/yediat/makeatest/core/metadata/processor/CompositeMetadataProcessor.java
@@ -0,0 +1,19 @@
+package com.yediat.makeatest.core.metadata.processor;
+
+import java.util.List;
+
+import com.yediat.makeatest.core.MakeATestAssertionError;
+
+public class CompositeMetadataProcessor implements MetadataProcessor {
+	
+	private List<MetadataProcessor> processors;
+
+	@Override
+	public void process() throws MakeATestAssertionError {
+		for(MetadataProcessor mp : processors){
+			System.out.println("mp.process: " + mp);
+			mp.process();
+		}
+	}
+
+}
